e9d4da52f2ccb626fed5267114268c282b1fa1bf
hyufoyzac <yzcc@qq.com>
2018-03-13 07:28:51 +0000

    Finish layout and routes

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -3,11 +3,12 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Http\Requests;
 
 class UsersController extends Controller
 {
     public function create()
     {
-    	return view('users.create');
+        return view('users.create');
     }
 }
